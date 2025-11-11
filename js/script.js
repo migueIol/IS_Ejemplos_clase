54709f3f0906c4e0e20b003f58c74df7f8542993
Miguel Chavez <miguel.chavez@fi.unam.edu>
2025-11-11 16:25:20 -0600

    ft: Se agrega ejemplo de eventos

--- js/script.js
+++ js/script.js
@@ -29,7 +29,27 @@ do{
 }while(m<4);
 
 const btnEstrella=document.getElementById("btnEstrella");
+
 btnEstrella.addEventListener("click", ()=> {
     cielo.innerHTML += "🌟"
 }
 )
+
+const btnPlaneta=document.getElementById("btnPlaneta");
+
+btnPlaneta.addEventListener("dblclick", ()=>{
+    cielo.innerHTML += "🪐";
+})
+
+const inputEmoji=document.getElementById("inputEmoji");
+
+const btnAgregarEmoji=document.getElementById("btnAgregarEmoji");
+btnAgregarEmoji.addEventListener("click", ()=>{
+    var emoji=inputEmoji.value.trim();
+    if(emoji) cielo.innerHTML+=emoji;
+    inputEmoji.value="";
+})
+
+inputEmoji.addEventListener("keydown", e=>{
+    if(e.key==="Escape") cielo.innerHTML="";
+})
